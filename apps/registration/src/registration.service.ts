c0c5680cdb5a09384e048b9476a083c2c48752f3
moutasem <bumoutasem@gmail.com>
2023-10-19 22:48:37 +0300

    lasttest

--- apps/registration/src/registration.service.ts
+++ apps/registration/src/registration.service.ts
@@ -26,7 +26,7 @@ export class RegistrationService {
       },
     });
     console.log(data);
-    return [{ arr: data }];
+    return data;
   }
 
   async getUserUnRegisterClasses(id): Promise<any> {
