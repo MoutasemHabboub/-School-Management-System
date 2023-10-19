4867d82e8b71ae69335d3d12ad7f6a009c36f104
moutasem <bumoutasem@gmail.com>
2023-10-19 14:11:51 +0300

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,13 +8,6 @@ module.exports = {
       args: 'run start:dev',
     },
     {
-      name: 'mo-api-class',
-      exec_mode: 'fork',
-      instances: 1, // Or a number of instances
-      script: 'pnpm',
-      args: 'run start:dev',
-    },
-    {
       name: 'mo-api-auth',
       exec_mode: 'fork',
       instances: 1, // Or a number of instances
