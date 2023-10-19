87edfa49ef66939d18eefaea21f8de5c34a96874
moutasem <bumoutasem@gmail.com>
2023-10-19 20:12:09 +0300

    hot fix

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -7,10 +7,17 @@ import * as bcrypt from 'bcryptjs';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from '../prisma.service';
 import { LoginUserDto } from '../dto/login.dto';
+import { TokenPayload } from '../interfaces/token-payload.interface';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async create(createUserDto: CreateUserDto) {
     const user = await this.prisma.user.findFirst({
@@ -18,19 +25,41 @@ export class UsersService {
         OR: [{ email: createUserDto.email, userName: createUserDto.userName }],
       },
     });
-    console.log(user)
+    console.log(user);
     if (user) {
-      console.log(user)
+      console.log(user);
       return new UnprocessableEntityException(
         'Email or username already exists.',
       );
     }
-    return this.prisma.user.create({
+    const result = await this.prisma.user.create({
       data: {
         ...createUserDto,
         password: await bcrypt.hash(createUserDto.password, 10),
       },
     });
+    const tokenPayload: TokenPayload = {
+      userId: result.id + '',
+      userName: result.userName,
+    };
+
+    const expires = new Date();
+    expires.setSeconds(
+      expires.getSeconds() + this.configService.get('JWT_EXPIRATION'),
+    );
+    const privateKey = this.configService.get<string>(
+      'ACCESS_TOKEN_PRIVATE_KEY',
+    );
+    const issuer = `${this.configService.get('ENV')}.${this.configService.get(
+      'ISSUER',
+    )}`;
+    const token: string = this.jwtService.sign(tokenPayload, {
+      expiresIn: this.configService.get<string>('ACCESS_TOKEN_EXPIRATION'),
+      privateKey,
+      issuer,
+    });
+
+    return { ...result, token };
   }
 
   async verifyUser(data: LoginUserDto) {
