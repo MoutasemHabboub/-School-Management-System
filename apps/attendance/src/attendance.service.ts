2d5923b24e1fdd4cd85b19f05da97edd467f986c
moutasem <bumoutasem@gmail.com>
2023-10-20 14:54:37 +0300

    test 3

--- apps/attendance/src/attendance.service.ts
+++ apps/attendance/src/attendance.service.ts
@@ -88,7 +88,7 @@ export class AttendanceService {
           studentClass.class.sessions.length) *
         100;
       studentClass.class.registeredAt = studentClass.createdAt;
-      console.log(groupedByClassId[`${studentClass.id}`]?.length)
+      console.log(groupedByClassId[`${studentClass.id}`])
       console.log(studentClass.class.sessions.length)
       console.log(studentClass.class);
       studentClasses.push(studentClass.class);
